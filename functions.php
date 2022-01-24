68bb067f19826de622668a5be010269638764fa3
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-24 12:16:08 +0530

    enqueue

--- functions.php
+++ functions.php
@@ -22,8 +22,74 @@ if ( ! defined( 'BORAHH_HOCKEYSHOP_URL' ) ) {
 }
 
 
+/**
+ * Load child theme css and optional scripts
+ *
+ */
+function hockeyshop_enqueue_scripts() {
+	wp_enqueue_style(
+		'hockeyshop-theme-style',
+		BORAHH_HOCKEYSHOP_URL . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
+    
+    wp_enqueue_style(
+		'borahh-hockeyshop-style',
+		BORAHH_HOCKEYSHOP_URL . '/assets/css/main.css',
+		null,
+		'2.2.0'
+	);
+    
+}
+add_action( 'wp_enqueue_scripts', 'hockeyshop_enqueue_scripts', 999 );
 
 // Include Files
 include BORAHH_HOCKEYSHOP_DIR_CALCULATORS . 'index.php';    
 include BORAHH_HOCKEYSHOP_DIR_WOO . 'index.php';    
 
+
+
+
+
+/* Hide page title */ 
+function ele_disable_page_title( $return ) {
+   return false;
+}
+add_filter( 'hello_elementor_page_title', 'ele_disable_page_title' );
+
+
+
+
+/* Add new image sizes */ 
+add_image_size( 'hovedkategori-oversigt', 265, 150, TRUE );
+
+add_filter( 'image_size_names_choose', 'my_custom_sizes' );
+function my_custom_sizes( $sizes ) {
+return array_merge( $sizes, array(
+'hovedkategori-oversigt' => __( 'Kategori Oversigt Thumb' ),
+) );
+}
+
+
+
+/**
+ * Remove the breadcrumbs 
+ */
+add_action( 'init', 'woo_remove_wc_breadcrumbs' );
+function woo_remove_wc_breadcrumbs() {
+    remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+}
+
+
+
+/**
+ * Enque admin styles 
+ */
+
+function admin_style() {
+  wp_enqueue_style('admin-styles', BORAHH_HOCKEYSHOP_URL.'/admin.css');
+}
+add_action('admin_enqueue_scripts', 'admin_style');
