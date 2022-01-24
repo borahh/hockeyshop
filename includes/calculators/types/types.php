02b21e21c99bced929e88d96085bb733e7d0903e
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-24 11:11:43 +0530

    ...

--- includes/calculators/types/types.php
+++ includes/calculators/types/types.php
@@ -1,3 +1,15 @@
 <?php 
 
-echo 'Should Work Now .............';
+echo 'Should Work Now Again .............';
+/*
+ *
+ *  Set the available calculators
+ * 
+ */
+
+function borahh_calculator_types() {
+    return (object) array(
+        'bauer_icehockey_skates' => 'BauerIcehockeySkates',
+    );
+}
+
