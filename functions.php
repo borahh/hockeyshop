24cbfc8ba426ae47fe2b6fd6e4269fb93944d533
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-25 19:31:52 +0530

    fixes

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ function hockeyshop_enqueue_scripts() {
 
 	wp_enqueue_script(
 		'borahh-single-product',
-		BORAHH_HOCKEYSHOP_JS_URL . 'single-product.js',
+		get_stylesheet_directory_uri(). '/assets/js/' . 'single-product.js',
 		array(),
 		'1.0.0',
         true
