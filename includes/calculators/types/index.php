8c404022ff3ef00b3dee39c0270281c881c4de36
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-25 12:31:31 +0530

    include

--- includes/calculators/types/index.php
+++ includes/calculators/types/index.php
@@ -1,4 +1,4 @@
 <?php
 
 include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'types.php';    
-// include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-bauer-icehockey-skates.php';    
+include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-bauer-icehockey-skates.php';    
