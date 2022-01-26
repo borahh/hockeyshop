0017a66943a6f1f965a37ad46d99227fc61567e0
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-26 18:40:55 +0530

    ...

--- includes/calculators/types/class-bauer-icehockey-skates.php
+++ includes/calculators/types/class-bauer-icehockey-skates.php
@@ -28,7 +28,7 @@ class BauerIcehockeySkates extends BorahhCalculatorBase {
                     <img class="calculator__wraper__content__form__tab--height__option__img_active" src="https://hockeyshop.dk/wp-content/themes/hockeyshop-theme/assets/images/foot-height-low-inactive.png" alt="" />
                     <img class="calculator__wraper__content__form__tab--height__option__img_inactive" src="https://hockeyshop.dk/wp-content/themes/hockeyshop-theme/assets/images/foot-height-low-active.png" alt="" />
                     <div class="calculator__wraper__content__form__tab--height__option__input"> 
-                        <input class="peer" type="radio" name="foot-height" value="0" ref="height0" />
+                        <input class="peer" type="radio" name="foot-height" value="0" x-ref="height0" />
                         <label @click="$refs.height0.click()"> 
                             <?php _e("Lav", "hockeyshop-theme"); ?>
                         </label>
@@ -39,7 +39,7 @@ class BauerIcehockeySkates extends BorahhCalculatorBase {
                     <img class="calculator__wraper__content__form__tab--height__option__img_active" src="https://hockeyshop.dk/wp-content/themes/hockeyshop-theme/assets/images/foot-height-low-inactive.png" alt="" />
                     <img class="calculator__wraper__content__form__tab--height__option__img_inactive" src="https://hockeyshop.dk/wp-content/themes/hockeyshop-theme/assets/images/foot-height-low-active.png" alt="" />
                     <div class="calculator__wraper__content__form__tab--height__option__input"> 
-                        <input class="peer" type="radio" name="foot-height" value="1" ref="height1" />
+                        <input class="peer" type="radio" name="foot-height" value="1" x-ref="height1" />
                         <label @click="$refs.height1.click()"> 
                             <?php _e("Mellem", "hockeyshop-theme"); ?>
                         </label>
@@ -50,7 +50,7 @@ class BauerIcehockeySkates extends BorahhCalculatorBase {
                     <img class="calculator__wraper__content__form__tab--height__option__img_active" src="https://hockeyshop.dk/wp-content/themes/hockeyshop-theme/assets/images/foot-height-low-inactive.png" alt="" />
                     <img class="calculator__wraper__content__form__tab--height__option__img_inactive" src="https://hockeyshop.dk/wp-content/themes/hockeyshop-theme/assets/images/foot-height-low-active.png" alt="" />
                     <div class="calculator__wraper__content__form__tab--height__option__input"> 
-                        <input class="peer" type="radio" name="foot-height" value="2" ref="height2" />
+                        <input class="peer" type="radio" name="foot-height" value="2" x-ref="height2" />
                         <label @click="$refs.height2.click()"> 
                             <?php _e("Høj", "hockeyshop-theme"); ?>
                         </label>
