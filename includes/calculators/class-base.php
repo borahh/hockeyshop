fe165705c5196f688688bae594b127d2e57f3fd5
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-26 09:27:26 +0530

    ...

--- includes/calculators/class-base.php
+++ includes/calculators/class-base.php
@@ -96,7 +96,7 @@ class BorahhCalculatorBase {
                     <!-- Calculator Nav -->
                     <div class="calculator__wraper__content__nav">
                         <!-- Previous Step -->
-                        <div class="calculator__wraper__content__nav__control" :class="showPrevHandler" @click="handlerPrev">
+                        <div class="calculator__wraper__content__nav__control" :class="showPrevHandler" @click="handlePrev">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path
                                 d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
