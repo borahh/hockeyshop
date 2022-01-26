c4829875d9bdf3b6c9e94031f7f005f933ded1d1
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-26 16:16:17 +0530

    fixed

--- includes/calculators/types/class-bauer-icehockey-skates.php
+++ includes/calculators/types/class-bauer-icehockey-skates.php
@@ -8,21 +8,21 @@ class BauerIcehockeySkates extends BorahhCalculatorBase {
      protected function steps() {
         ob_start();
         ?>
-            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 0">Extend this method to include steps 1</div>
-            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 1">
-                <h1>
+            <div class="calculator__wraper__content__form__tab calculator__wraper__content__form__tab--length" x-show="currentStep === 0">Extend this method to include steps 1</div>
+            <div class="calculator__wraper__content__form__tab calculator__wraper__content__form__tab--fit" x-show="currentStep === 1">
+                <h2>
                     <?php _e("Vælg dit fit", "hockeyshop-theme"); ?>
-                </h1>
+                </h2>
             </div>
-            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 2">
-                <h1>
+            <div class="calculator__wraper__content__form__tab calculator__wraper__content__form__tab--width" x-show="currentStep === 2">
+                <h2>
                     <?php _e("Vælg din fodbredde", "hockeyshop-theme"); ?>
-                </h1>
+                </h2>
             </div>
-            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 3">
-                <h1>
+            <div class="calculator__wraper__content__form__tab calculator__wraper__content__form__tab--height" x-show="currentStep === 3">
+                <h2>
                     <?php _e("Væld din fodhøjde", "hockeyshop-theme"); ?>
-                </h1>
+                </h2>
             </div>
         <?php
         return ob_get_clean();
