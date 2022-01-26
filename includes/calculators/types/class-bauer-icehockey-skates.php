7bb82ae5a2b59cc2a5ae4d833d41e20d27461587
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-26 16:08:58 +0530

    ...

--- includes/calculators/types/class-bauer-icehockey-skates.php
+++ includes/calculators/types/class-bauer-icehockey-skates.php
@@ -3,4 +3,28 @@ class BauerIcehockeySkates extends BorahhCalculatorBase {
     public function __construct($dependencies) {
         $this->dependencies = $dependencies;
     }
+
+     // Steps 
+     protected function steps() {
+        ob_start();
+        ?>
+            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 0">Extend this method to include steps 1</div>
+            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 1">
+                <h1>
+                    <?php _e("Vælg dit fit", "hockeyshop-theme"); ?>
+                </h1>
+            </div>
+            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 2">
+                <h1>
+                    <?php _e("Vælg din fodbredde", "hockeyshop-theme"); ?>
+                </h1>
+            </div>
+            <div class="calculator__wraper__content__form__tab" x-show="currentStep === 3">
+                <h1>
+                    <?php _e("Væld din fodhøjde", "hockeyshop-theme"); ?>
+                </h1>
+            </div>
+        <?php
+        return ob_get_clean();
+    }
 }
