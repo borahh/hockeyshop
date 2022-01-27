dcb0b2688b0b1056ef9079d7c92a04361d475193
Himanshu Borah <borahhimanshu3@gmail.com>
2022-01-27 12:18:54 +0530

    ...

--- includes/calculators/types/class-bauer-icehockey-skates.php
+++ includes/calculators/types/class-bauer-icehockey-skates.php
@@ -11,11 +11,9 @@ class BauerIcehockeySkates extends BorahhCalculatorBase {
         foreach($options as $index=>$option) {
         ?>
         <div>
-            <input class="peer" type="radio" name="foot-width" id="<?php echo 'width_' . $option; ?>" x-ref="<?php echo 'width' . $option; ?>" value="<?php echo $index; ?>" <?php if ($index === 0) echo "checked" ;?>/>
-            <div class="img_option_wraper">
-                <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'foot-width-' . $option . '-inactive.webp'; ?>" alt="" />
-                <img class="img_active" :class="$refs.<?php echo 'width' . $option; ?>.checked ? 'opacity-100' : 'opacity-0'" src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'foot-width-' . $option . '-active.webp'; ?>" alt="" />
-            </div>
+            <input class="peer" type="radio" name="foot-width" id="<?php echo 'width_' . $option; ?>" value="<?php echo $index; ?>" <?php if ($index === 0) echo "checked" ;?>/>        
+            <img class="peer-checked:hidden" src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'foot-width-' . $option . '-inactive.webp'; ?>" alt="" />
+            <img class="hidden peer-checked:block" src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'foot-width-' . $option . '-active.webp'; ?>" alt="" />
             <label for="<?php echo 'width_' . $option; ?>" class="peer-checked:bg-accent peer-checked:text-white"> 
                 <?php _e($labels[$index], "hockeyshop-theme"); ?>
             </label>
