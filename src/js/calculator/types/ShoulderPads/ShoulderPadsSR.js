ceee0b8a292081269d2a57a97b213c6a2f0b1db6
Himanshu Borah <borahhimanshu3@gmail.com>
2022-02-10 22:32:23 +0530

    Fixed

--- src/js/calculator/types/ShoulderPads/ShoulderPadsSR.js
+++ src/js/calculator/types/ShoulderPads/ShoulderPadsSR.js
@@ -7,13 +7,32 @@ export function ShoulderPadsSR() {
       chest: 86,
       height: 157,
     },
+    getHeight(i) {
+      const ranges = {
+        a: [86, 97],
+        b: [94, 104],
+        c: [102, 112],
+        d: [109, 122],
+      };
+      const chest = this.invert(parseInt(this.input.chest, 10));
+
+      if (chest >= 86 && chest <= 97) {
+        return ranges.a[i];
+      } else if (chest >= 94 && chest <= 108) {
+        return ranges.b[i];
+      } else if (chest >= 102 && chest <= 112) {
+        return ranges.c[i];
+      } else if (chest >= 109 && chest <= 122) {
+        return ranges.d[i];
+      }
+    },
     rangeFrom: {
       chest: 86,
-      height: 157,
+      height: getHeight(0),
     },
     rangeTo: {
       chest: 112,
-      height: 188,
+      height: getHeight(1),
     },
     getResult() {
       const chest = this.invert(parseInt(this.input.chest, 10));
