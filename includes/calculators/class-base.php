49cf7b40af58fd5bf194ea340e7c8a956202b64b
Himanshu Borah <borahhimanshu3@gmail.com>
2022-02-14 18:35:52 +0530

    Fixes

--- includes/calculators/class-base.php
+++ includes/calculators/class-base.php
@@ -89,9 +89,9 @@ class BorahhCalculatorBase {
         <div class="calculator__wraper__content__end__unmatched__content">
             <p><?php _e("Din anbefalede størrelse er", "hockeyshop-theme"); ?></p>
             <div>
-                <span x-text="dataObtained.fit"></span>
-                <span x-text="dataObtained.scale"></span>
-                <span x-text="dataObtained.size"></span>
+                <span x-text="dataObtained.fit" x-show="dataObtained.fit"></span>
+                <span x-text="dataObtained.scale" x-show="dataObtained.scale"></span>
+                <span x-text="dataObtained.size" x-show="dataObtained.size"></span>
             </div>
         </div>
         
