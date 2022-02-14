f1c6a606739c6f18407544221f0af252871a75eb
Himanshu Borah <borahhimanshu3@gmail.com>
2022-02-14 22:01:40 +0530

    ...

--- includes/calculators/types/class-shoulder-pads.php
+++ includes/calculators/types/class-shoulder-pads.php
@@ -63,11 +63,11 @@ class ShoulderPads extends BorahhCalculatorBase {
             <?php _e("Brystomkreds", "hockeyshop-theme"); ?>
         </h2>
         <div class="calculator__wraper__content__form__tab--length__input">
-            <input type="text" :value="$watch('input.chest', value =>value)">            
+            <input type="text" :value="getChestValue()" >            
             <span x-text="selectedUnit"></span>
         </div>
         
-        <input type="range" class="calculator-range sm:w-5/6" x-bind:min="getChestInput(0)" step="1" x-bind:max="getChestInput(1)" x-model="input.chest" :value="$el.getAttribute('min')">
+        <input type="range" class="calculator-range sm:w-5/6" x-bind:min="getChestInput(0)" step="1" x-bind:max="getChestInput(1)" x-model="input.chest" :value="$el.getAttribute('min')" x-effect="$watch('input.chest', value => console.log(value))">
         
         
         <?php
