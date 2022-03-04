18c24d756aa2a56e9ea07474dc15797fb150e364
Himanshu Borah <borahhimanshu3@gmail.com>
2022-03-04 20:17:28 +0530

    Include Elbow Pads

--- includes/calculators/types/index.php
+++ includes/calculators/types/index.php
@@ -5,3 +5,4 @@ include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-bauer-icehockey-skates.
 include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-ccm-skates.php';    
 include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-ccm-gloves.php';    
 include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-ccm-shoulder-pads.php';    
+include BORAHH_HOCKEYSHOP_DIR_CALCULATORS_TYPES . 'class-ccm-elbow-pads.php';    
