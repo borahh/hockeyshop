9184fa447eb9d5276d6de0e83b85577f34410a93
Himanshu Borah <borahhimanshu3@gmail.com>
2022-03-19 17:06:35 +0530

    Bauer Types

--- includes/calculators/types/types.php
+++ includes/calculators/types/types.php
@@ -54,6 +54,8 @@ function borahh_calculator_types() {
                )
             ]
         ),
+
+        /** CCM Gloves **/
         'ccm_gloves_sr' => (object) array(
             'ID' => 'CCMGloves',
             'type' => 'SR',
@@ -93,6 +95,62 @@ function borahh_calculator_types() {
                )
             ]
         ),
+
+        /** Bauer Gloves **/
+        'bauer_gloves_sr' => (object) array(
+            'ID' => 'BauerGloves',
+            'type' => 'SR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+        'bauer_gloves_jr' => (object) array(
+            'ID' => 'BauerGloves',
+            'type' => 'JR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+        'bauer_gloves_yt' => (object) array(
+            'ID' => 'BauerGloves',
+            'type' => 'YT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+        'bauer_gloves_int' => (object) array(
+            'ID' => 'BauerGloves',
+            'type' => 'INT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+
+        /** CCM Shoulder Pads **/
         'ccm_shoulder_pads_jr' => (object) array(
             'ID' => 'CCMShoulderPads',
             'type' => 'JR',
@@ -145,6 +203,75 @@ function borahh_calculator_types() {
                )
             ]
         ),
+
+        /** Bauer Shoulder Pads **/
+        'bauer_shoulder_pads_jr' => (object) array(
+            'ID' => 'BauerShoulderPads',
+            'type' => 'JR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_shoulder_pads_int' => (object) array(
+            'ID' => 'BauerShoulderPads',
+            'type' => 'INT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_shoulder_pads_sr' => (object) array(
+            'ID' => 'BauerShoulderPads',
+            'type' => 'SR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_shoulder_pads_yt' => (object) array(
+            'ID' => 'BauerhoulderPads',
+            'type' => 'YT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_shoulder_pads_women' => (object) array(
+            'ID' => 'BauerShoulderPads',
+            'type' => 'WOMEN',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+
+        /** CCM Elbow Pads **/
         'ccm_elbow_pads_sr' => (object) array(
             'ID' => 'CCMElbowPads',
             'type' => 'SR',
@@ -184,6 +311,62 @@ function borahh_calculator_types() {
                )
             ]
         ),
+
+        /** Bauer Elbow Pads **/
+        'bauer_elbow_pads_sr' => (object) array(
+            'ID' => 'BauerElbowPads',
+            'type' => 'SR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_elbow_pads_jr' => (object) array(
+            'ID' => 'BauerElbowPads',
+            'type' => 'JR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_elbow_pads_yt' => (object) array(
+            'ID' => 'BauerElbowPads',
+            'type' => 'YT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_elbow_pads_int' => (object) array(
+            'ID' => 'BauerElbowPads',
+            'type' => 'INT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+
+        /** CCM Shin Guards */
         'ccm_shin_guards_sr' => (object) array(
             'ID' => 'CCMShinGuards',
             'type' => 'SR',
@@ -223,6 +406,63 @@ function borahh_calculator_types() {
                )
             ]
         ),
+
+        /** Bauer Shin Guards */
+        'bauer_shin_guards_sr' => (object) array(
+            'ID' => 'BauerShinGuards',
+            'type' => 'SR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+        'bauer_shin_guards_jr' => (object) array(
+            'ID' => 'BauerShinGuards',
+            'type' => 'JR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+        'bauer_shin_guards_yt' => (object) array(
+            'ID' => 'BauerShinGuards',
+            'type' => 'YT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+        'bauer_shin_guards_int' => (object) array(
+            'ID' => 'BauerShinGuards',
+            'type' => 'INT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_tommer'),
+                    'get'  => 'slug',
+                    'selector' => 'data-value'
+               )
+            ]
+        ),
+
+
+        /** CCM Player Pants */
         'ccm_player_pants_sr' => (object) array(
             'ID' => 'CCMPlayerPants',
             'type' => 'SR',
@@ -264,6 +504,73 @@ function borahh_calculator_types() {
         ),
         'ccm_player_pants_women' => (object) array(
             'ID' => 'CCMPlayerPants',
+            'type' => 'WOMEN',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+
+        /** Bauer Player Pants */
+        'bauer_player_pants_sr' => (object) array(
+            'ID' => 'BauerPlayerPants',
+            'type' => 'SR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_player_pants_jr' => (object) array(
+            'ID' => 'BauerPlayerPants',
+            'type' => 'JR',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_player_pants_int' => (object) array(
+            'ID' => 'BauerPlayerPants',
+            'type' => 'INT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_player_pants_yt' => (object) array(
+            'ID' => 'BauerPlayerPants',
+            'type' => 'YT',
+            'fields' => null,
+            'dependencies' => [
+                (object) array(
+                    'name' => 'size',
+                    'list' =>  array('pa_x-stoerrelser'),
+                    'get'  => 'name',
+                    'selector' => 'data-title'
+               )
+            ]
+        ),
+        'bauer_player_pants_women' => (object) array(
+            'ID' => 'BauerPlayerPants',
             'type' => 'WOMEN',
             'fields' => null,
             'dependencies' => [
