a40e9f9d8771472c04118f7575071f9737415068
Himanshu Borah <borahhimanshu3@gmail.com>
2022-05-15 17:31:02 +0530

    ...

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -8,7 +8,7 @@ function foobar_func( $atts ){ ?>
         
         <?php while( have_rows('hbdv_mega_primary_levels', 'option') ): the_row(); ?>
         
-            <li><?php the_sub_field('hbdv_mega_primary_level_title'); ?></li>
+            <li><a href="<?php the_sub_field('hbdv_mega_primary_level_url'); ?>"><?php the_sub_field('hbdv_mega_primary_level_title'); ?></a></li>
         
         <?php endwhile; ?>
         
