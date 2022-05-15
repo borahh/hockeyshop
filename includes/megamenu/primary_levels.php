0cd0dbe3d4893d8a08e45c76c28fc68361bb8406
Himanshu Borah <borahhimanshu3@gmail.com>
2022-05-15 17:25:45 +0530

    short

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -1,2 +1,24 @@
 <?php
 
+//[foobar]
+function foobar_func( $atts ){ ?>
+	<?php if( have_rows('repeater', 'option') ): ?>
+
+        <ul>
+        
+        <?php while( have_rows('repeater', 'option') ): the_row(); ?>
+        
+            <li><?php the_sub_field('title'); ?></li>
+        
+        <?php endwhile; ?>
+        
+        </ul>
+        
+        <?php endif; ?>
+        <?php
+}
+add_shortcode( 'foobar', 'foobar_func' );
+
+
+
+
