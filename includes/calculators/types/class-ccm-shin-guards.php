19ab731a7ee07cf67e744417e52a4087efa5adab
Himanshu Borah <borahhimanshu3@gmail.com>
2022-05-20 19:12:12 +0530

    ...

--- includes/calculators/types/class-ccm-shin-guards.php
+++ includes/calculators/types/class-ccm-shin-guards.php
@@ -63,7 +63,7 @@ class CCMShinGuards extends BorahhCalculatorBase {
         // Fix from here
         ?>
         
-        <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'shin.svg'; ?>" class="small"> 
+        <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'shin.svg'; ?>" class="vsmall"> 
 
         <h2>
             <?php _e("Underben", "hockeyshop-theme"); ?>
