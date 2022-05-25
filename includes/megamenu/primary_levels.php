81fb8bb88160d887e30d67cbcb8d95d3ed4ade09
Himanshu Borah <borahhimanshu3@gmail.com>
2022-05-25 18:41:27 +0530

    upd

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -10,25 +10,11 @@ function get_hockeyshop_primary_levels() {
     endif;
     return $levels;
 }
-add_shortcode( 'foobar', 'get_hockeyshop_primary_levels' );
 
 
-//[foobar]
-function foobar_func( $atts ){ ?>
-	<?php if( have_rows('hbdv_mega_primary_levels', 'option') ): ?>
 
-        <ul>
-        
-        <?php while( have_rows('hbdv_mega_primary_levels', 'option') ): the_row(); ?>
-        
-            <li><a href="<?php the_sub_field('hbdv_mega_primary_level_url'); ?>"><?php the_sub_field('hbdv_mega_primary_level_title'); ?></a></li>
-        
-        <?php endwhile; ?>
-        
-        </ul>
-        
-        <?php endif; ?>
-        <?php
+function foobar_func( $atts ){ 
+    print_r(get_hockeyshop_primary_levels());
 }
 add_shortcode( 'foobarx', 'foobar_func' );
 
