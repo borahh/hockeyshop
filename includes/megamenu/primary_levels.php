6d5c9c49046c0ce5bf1800e17d8c2597b368694e
Dinesh485 <ghantasaladinesh@gmail.com>
2022-05-27 17:25:50 +0530

    test

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -6,7 +6,17 @@ function hockeyshop_create_menu_name($level) {
 
 function get_hockeyshop_menu_holder() {
     ?>
-    <div class="hbdv_megamenu relative  p-5 lg:static  flex-grow lg:flex  lg:p-0 lg:justify-center lg:items-center lg:w-full">
+     <div class="menu item  overflow-y-scroll lg:overflow-visible overflow-x-hidden  flex flex-col lg:bg-[#1d1d1d] ">
+                <div class=' p-3 px-5 flex justify-between lg:hidden'>
+                    <button class="flex items-center gap-2 hidden" id='back_btn'>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+                        </svg>
+                        Back
+                    </button>
+                </div>
+               <div class="hbdv_megamenu relative  p-5 lg:static  flex-grow lg:flex  lg:p-0 lg:justify-center lg:items-center lg:w-full">
                 <?php
                 if( have_rows('hbdv_mega_primary_levels', 'option') ):  
                     while( have_rows('hbdv_mega_primary_levels', 'option') ): the_row();
@@ -31,7 +41,7 @@ function get_hockeyshop_menu_holder() {
                 endif;
                 ?>
             </div>
-
+            </div>
 
     <?php
 
