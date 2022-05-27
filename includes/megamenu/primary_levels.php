2579b3daf142e5f56d382da2897407899fa785e0
Himanshu Borah <borahhimanshu3@gmail.com>
2022-05-27 11:30:39 +0530

    ...

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -16,20 +16,15 @@ function get_hockeyshop_main_menu() { ?>
     <div class="hbdv_megamenu--primary">
     <?php
     if( have_rows('hbdv_mega_primary_levels', 'option') ):  
-        ?>
-        <ul class="menu">
-        <?php
         while( have_rows('hbdv_mega_primary_levels', 'option') ): the_row();
            $url=get_sub_field('hbdv_mega_primary_level_url');
            $title=get_sub_field('hbdv_mega_primary_level_title');
            ?>
-            <li class="menu-item menu-item-type-custom menu-item-object-custom">
+            
               <a href="<?php echo $url; ?>"><?php echo $title; ?></a>
-            </li>
+            
            <?php    
-        endwhile;  ?>
-        <ul class="menu">
-        <?php
+        endwhile;
     endif;
     ?>
     </div>
