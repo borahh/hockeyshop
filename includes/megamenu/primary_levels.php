db96c0d388a66e5ed6b3dd63a387a373927410ff
Dinesh485 <ghantasaladinesh@gmail.com>
2022-05-28 11:30:10 +0530

    .

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -89,17 +89,13 @@ function get_hockeyshop_menu_holder() {
                     endwhile;
                 endif;
                 ?>
-                 <div class="absolute bottom-0 left-0 w-full bg-gradient-to-t from-white via-white to-transparent text-xl font-bold grid grid-cols-2 lg:hidden p-4 font-['Pathway_Gothic_One'] ">
+                 <div class="absolute bottom-0 left-0 w-full bg-gradient-to-t from-white via-white to-transparent text-xl font-bold grid grid-cols-2 lg:hidden p-4 font-social-gothic ">
                          <a href="#" class = 'flex flex-col items-center'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
-                              </svg>
+                             <img src="/assets/images/currency.png" alt="icon" class='w-6'>
                               Vault
                          </a>
                          <a href="#" class = 'flex flex-col items-center'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                              </svg>
+                              <img src="/assets/images/user.png" alt="icon" class='w-6'>
                               My Account
                          </a>
                     </div>
