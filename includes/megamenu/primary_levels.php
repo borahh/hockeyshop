5ce52ee9f6551017dd0ab7a703e195e95c5035e6
Dinesh485 <ghantasaladinesh@gmail.com>
2022-05-30 18:03:13 +0530

    .

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -14,7 +14,7 @@ function get_hockeyshop_menu_holder() {
               </div>
         </div>
     <div class="flex items-center justify-between lg:block">
-     <div class="flex items-center justify-between flex-grow gap-2 p-3 pr-0 text-white lg:pr-5 2xl:p-8 lg:grid lg:grid-cols-3 lg:justify-items-center w-11/12 max-w-[1500px] mx-auto">
+     <div class="flex items-center justify-between flex-grow gap-2 p-3 pr-0 text-white lg:pr-5 2xl:py-8 lg:grid lg:grid-cols-3 lg:justify-items-center w-11/12 max-w-[1500px] mx-auto">
                 <img class="w-[90px] lg:w-[200px] xl:w-[250px] lg:order-2" src="https://hockeyshop.dk/wp-content/uploads/2020/07/HockeyShop.svg" alt="">
 
 
