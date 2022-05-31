aef980569989cfda7df8e751933b423697b0bd42
Dinesh485 <ghantasaladinesh@gmail.com>
2022-05-31 18:00:57 +0530

    .

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -37,7 +37,7 @@ function get_hockeyshop_menu_holder() {
         </div>
     <div class="flex items-center justify-between lg:block">
      <div class="flex items-center justify-between flex-grow gap-2 p-3 pr-0 text-white lg:pr-5 2xl:py-8 lg:grid lg:grid-cols-3 lg:justify-items-center w-11/12 max-w-[1500px] mx-auto">
-                <img class="w-[90px] lg:w-[200px] xl:w-[250px] lg:order-2" src="https://hockeyshop.dk/wp-content/uploads/2020/07/HockeyShop.svg" alt="">
+                <img class="w-[90px] lg:w-[200px] xl:w-[230px] lg:order-2" src="https://hockeyshop.dk/wp-content/uploads/2020/07/HockeyShop.svg" alt="">
 
                 <div class = 'search_bar group justify-self-start '>
                 <div class = 'input_field group-hover:scale-x-100 bg-white lg:bg-[#2f2f2f]'>
@@ -50,7 +50,7 @@ function get_hockeyshop_menu_holder() {
                 </div>
                 
 
-                <div class="flex items-center order-3 text-base font-bold text-white 2xl:text-xl font-social-gothic">
+                <div class="flex items-center order-3 text-base font-bold text-white  font-social-gothic">
                     <a href="#" class="hidden lg:block">Login</a>
                     <hr class='hidden lg:block h-5 border-[1px] border-solid mx-[26px] border-gray-500/50'>
                     <a href="#" class="hidden lg:block">Vault</a>
