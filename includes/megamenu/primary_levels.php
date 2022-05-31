52ea0e855a7096a0066ce91f0f60954b55e5f7e8
Dinesh485 <ghantasaladinesh@gmail.com>
2022-05-31 18:48:01 +0530

    .

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -222,7 +222,7 @@ function get_hockeyshop_main_menu() { ?>
 
         const dropDownBtn = document.querySelectorAll('.drop_down > .btn')
         dropDownBtn.forEach(item =>{
-            item.insertAdjacentHTML('beforeend',  `<svg xmlns="http://www.w3.org/2000/svg" class="w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            item.insertAdjacentHTML('beforeend',  `<svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
  <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
 </svg>`)
         })   
