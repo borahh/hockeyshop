1eaf6405235b144ccad0eed26acfea72bba7332c
Dinesh485 <ghantasaladinesh@gmail.com>
2022-05-31 16:55:20 +0530

    .

--- includes/megamenu/primary_levels.php
+++ includes/megamenu/primary_levels.php
@@ -50,7 +50,7 @@ function get_hockeyshop_menu_holder() {
                 </div>
                 
 
-                <div class="flex items-center order-3 text-lg font-medium text-white 2xl:text-xl font-social-gothic">
+                <div class="flex items-center order-3 text-base font-medium text-white 2xl:text-xl font-social-gothic">
                     <a href="#" class="hidden lg:block">Login</a>
                     <hr class='hidden lg:block h-5 border-[1px] border-solid mx-5 border-gray-500/50'>
                     <a href="#" class="hidden lg:block">Vault</a>
@@ -118,11 +118,11 @@ function get_hockeyshop_menu_holder() {
                 ?>
                  <div class="absolute bottom-0 left-0 grid w-full grid-cols-2 p-4 font-bold bg-gradient-to-t from-white via-white to-transparent lg:hidden font-social-gothic ">
                          <a href="#" class = 'flex flex-col items-center'>
-                             <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'currency.png'; ?>" alt="icon" class='w-10 mb-2'>
+                             <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'currency.png'; ?>" alt="icon" class='w-[24px] mb-2'>
                               Vault
                          </a>
                          <a href="#" class = 'flex flex-col items-center'>
-                              <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'user.png'; ?>" alt="icon" class='w-10 mb-2'>
+                              <img src="<?php echo BORAHH_HOCKEYSHOP_IMG_URL . 'user.png'; ?>" alt="icon" class='w-[30px] mb-2'>
                               My Account
                          </a>
                     </div>
